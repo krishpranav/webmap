d7a21604e6e811f3fde69bb4c2b43c93b108708a
krishpranav <krisna.pranav@gmail.com>
2021-06-04 12:57:12 +0530

    [+] Added some app functions [+]

--- app.js
+++ app.js
@@ -9,3 +9,14 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 
 var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
