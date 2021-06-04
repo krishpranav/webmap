048734f400ff297ec5c47a68b480b813ddb35b65
krishpranav <krisna.pranav@gmail.com>
2021-06-04 12:53:20 +0530

    [+] Added some var [+]

---
+++ app.js
@@ -0,0 +1,11 @@
+// var imports
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+
+var app = express();
