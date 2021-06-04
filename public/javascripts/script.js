a15d9e914208391d740ac372a826878b351eddf0
krishpranav <krisna.pranav@gmail.com>
2021-06-04 12:43:14 +0530

    [+] body script updated [+]

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,5 +6,16 @@ $(document).ready(function() {
         action();
     });
 
-    
+    $("body").on("click", ".list-group-item", function(e) {
+        e.stopPropagation();
+        if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
+            let index = options.indexOf($(this).attr('value'));
+            options.splice(index, 1);
+        }
+        else {
+            $(this).addClass("active");
+            options.push($(this).attr(value))
+        }
+    })
 })
